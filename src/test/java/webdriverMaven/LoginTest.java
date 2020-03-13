d6e3b43322f3b7e1b1bfd7001bd2f4f50cc7f721
priya <mpbhagyavi223@gmail.com>
2020-03-14 00:53:19 +0530

    commit

---
+++ src/test/java/webdriverMaven/LoginTest.java
@@ -0,0 +1,33 @@
+package webdriverMaven;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+public class LoginTest {
+	
+	public static WebDriver driver;
+	
+	@BeforeSuite
+	public void setUp() {
+		System.setProperty("webdriver.chrome.driver", "C:\\Selenium Drivers\\chromedriver.exe");
+		driver = new ChromeDriver();
+	}
+	
+	@Test
+	public void doLogin() {
+		
+		driver.get("http://gmail.com");
+		driver.findElement(By.id("identifierId")).sendKeys("trainer@way2automation.com");
+		
+	}
+	
+	@AfterSuite
+	public void tearown() {
+		driver.quit();
+	}
+
+}
